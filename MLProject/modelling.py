020ede8c9625b3fe342f4d0f19e68d9025ec0d53
Azizah Salma <147806636+almaayunisaa@users.noreply.github.com>
2025-11-29 12:28:57 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,16 +1,24 @@
 # file modelling.py
 import pandas as pd
 import mlflow
-from preprocessing.automated_AzizahSalmaAyunisaPurnomo import automate_Azizah
 from sklearn.ensemble import RandomForestClassifier
+import os
+import sys
+
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(curr_dir)
+sys.path.append(parent_dir)
+
+from preprocessing import automate_Azizah
 
 # Create a new MLflow Experiment
 mlflow.set_experiment("Latihan Model Statis")
 
-df = pd.read_csv("Eksperimen_SML_Azizah_Salma_Ayunisa_Purnomo/PCOS_raw.csv")
-save_path = "Eksperimen_SML_Azizah_Salma_Ayunisa_Purnomo/preprocessing/preprocessing.joblib"
-file_path = "Eksperimen_SML_Azizah_Salma_Ayunisa_Purnomo/preprocessing/PCOS_preprocessing.csv"
-X_train, X_test, y_train, y_test = automate_Azizah(df, save_path, file_path)
+df = pd.read_csv("../PCOS_raw.csv")
+save_path = "preprocessing.joblib"
+file_path_header = "PCOS_preprocessing_header.csv"
+file_path_data = "PCOS_preprocessing.csv"
+X_train, X_test, y_train, y_test = automate_Azizah(df, save_path, file_path_header, file_path_data)
 
 input_example = X_train[0:5]
 
@@ -24,15 +32,13 @@ with mlflow.start_run():
 
     # Initialize RandomForestClassifier
     rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, min_samples_split=min_samples_split, min_samples_leaf=min_samples_leaf)
-
+    rf.fit(X_train, y_train)
     # Logging model
     mlflow.sklearn.log_model(
         sk_model=rf,
         artifact_path="model",
         input_example=input_example
     )
-
-    rf.fit(X_train, y_train)
 
     accuracy = rf.score(X_test, y_test)
     mlflow.log_metric("accuracy", accuracy)
